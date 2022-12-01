1eca2b7f5548e8c0bc9ed576f107a342085f1931
Ross Fleming <rossflem@icloud.com>
2022-12-01 19:38:50 +0000

    Completed day 1

---
+++ day-1/calories.py
@@ -0,0 +1,30 @@
+
+elf_list = []
+
+puzzleInput = open('input.txt', 'r')
+
+count = 0
+total = 0
+
+while True:
+    count += 1
+
+    line = puzzleInput.readline()
+
+    # Loop until end of file
+    if not line:
+        break
+
+    # Strip new line and add to running total
+    if line != '\n':
+        total += int(line.strip())
+    else:
+        elf_list.append(total)
+
+        # Reset the total when a new line is reached
+        total = 0
+
+puzzleInput.close()
+
+print("The top elf is carrying " + str(max(elf_list)) + " calories.")
+print("The top 3 elves are carrying " + str(sum(sorted(elf_list)[-3:])))
